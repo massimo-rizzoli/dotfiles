2c0611932ecf7765d017dcc127201bcbfbdfe836
Massimo Rizzoli <massimo.rizzoli99@gmail.com>
2022-07-24 12:15:45 +0200

    fix org bullets

--- nvim/.config/nvim/lua/mylua/org-bullets.lua
+++ nvim/.config/nvim/lua/mylua/org-bullets.lua
@@ -1,8 +1,15 @@
-require("org-bullets").setup {
-  symbols = { "", "", "◉", "○" }
-  -- or a function that receives the defaults and returns a list
-  --symbols = function(default_list)
-  --            table.insert(default_list, "♥")
-  --            return default_list
-  --          end
+require("org-bullets").setup{
+  symbols ={
+    headlines={ "", "", "◉", "○" },
+    -- or a function that receives the defaults and returns a list
+    --headlines = function(default_list)
+    --  table.insert(default_list, "♥")
+    --  return default_list
+    --end,
+    checkboxes = {
+      cancelled = { "", "OrgCancelled" },
+      done = { "✓", "OrgDone" },
+      todo = { "˟", "OrgTODO" },
+    },
+  }
 }
